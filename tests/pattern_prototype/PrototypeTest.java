6ea64e8f5a3391a7816bccf7e631f7f9bf9513a7
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2021-11-09 23:53:20 +0330

    add test for prototype pattern

---
+++ tests/pattern_prototype/PrototypeTest.java
@@ -0,0 +1,29 @@
+package pattern_prototype;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class PrototypeTest {
+    @Test
+    public void clone_rectangle() {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(2);
+        rectangle.setHeight(2);
+        rectangle.setColor(Color.BLUE);
+        Rectangle rectangle1 = rectangle.copy();
+        Assert.assertEquals(rectangle.getWidth(), rectangle1.getWidth());
+        Assert.assertEquals(rectangle.getHeight(), rectangle1.getHeight());
+        Assert.assertEquals(rectangle.getColor(), rectangle1.getColor());
+    }
+
+    @Test
+    public void clone_circle() {
+        Circle circle = new Circle();
+        circle.setRadius(2);
+        circle.setColor(Color.RED);
+        Circle circle1 = circle.copy();
+        Assert.assertEquals(circle.getRadius(), circle1.getRadius());
+        Assert.assertEquals(circle.getColor(), circle1.getColor());
+    }
+}
